1714925a95cbccff08e6af7fe961a77fdb120f27
Documenter.jl <documenter@juliadocs.github.io>
2026-04-25 13:00:25 +0000

    build based on 7c07c8b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#CBChPT","page":"Home","text":"Documentation for CBChPT.\n\n","title":"CBChPT"},{"category":"method","location":"#CBChPT.CBnlo-Tuple{Float64, Float64, Float64}","page":"Home","text":"b0, bD, bF have the unit GeV^-1. b0 = -0.665, bD = 0.062, bF = -0.354\n\n\n\n\n\n","title":"CBChPT.CBnlo"},{"category":"method","location":"#CBChPT.mB_one_loop-Tuple{Vector{Float64}, Vector{Float64}, Float64}","page":"Home","text":"(p^3)\n\n\n\n\n\n","title":"CBChPT.mB_one_loop"}]
+}
